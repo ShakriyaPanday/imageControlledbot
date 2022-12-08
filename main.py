47674e6097b3f7da47d558b01514ef8f9eb4b0e8
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-12-08 14:02:32 +0545

    all function added

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('Shakriya')
+import cv2
+
+def arrowDetection():
+    return 0
+
+def getDistance():
+    distance =0
+    return distance
+
+def moveForward():
+    return 0
+
+def moveBackward():
+    return 0
+
+def turnLeft():
+    return 0
+
+def turnRight():
+    return 0
+
+def getColor():
+    color = 'red'
+    return color
+
+def findContours():
+    contour =[]
+    return contour
+
+def dectectShape():
+    shape='triange'
+    return shape
+def linefollwing():
+    linefollw = 1
