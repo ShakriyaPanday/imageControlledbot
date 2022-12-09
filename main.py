fe8697a0d145528ddd0d3bde08d5c1a20bc0810f
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-12-09 23:07:12 +0545

    finadcontour added

--- main.py
+++ main.py
@@ -23,12 +23,29 @@ def getColor():
     color = 'red'
     return color
 
-def findContours():
-    contour =[]
-    return contour
+def findContours(img):
+    _, threshold = cv2.threshold(img, 
+    127, 255, cv2.THRESH_BINARY)
+    contours,_ = cv2.findContours(
+	threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
 def dectectShape():
     shape='triange'
     return shape
+
 def linefollwing():
     linefollw = 1
+
+def obstacleDetect():
+    turnLeft()
+
+def turnDirection(direction):
+    if(direction=='left'):
+        turnLeft()
+    elif(direction=='right'):
+        turnRight()
+    elif(direction=='forward'):
+        moveForward()
+    elif(direction=='backward'):
+        moveBackward()
